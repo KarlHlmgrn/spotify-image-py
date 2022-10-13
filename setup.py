6e92381f4ecfc64d9349e8621d744f0598762adf
KarlHlmgrn <karl.holmgren4@gmail.com>
2022-10-13 22:00:39 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,24 +1,26 @@
 from setuptools import setup, find_packages
-from codecs import open
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
-def read_requirements(filename):
-    with open(filename, "r", encoding="utf-8") as fp:
-        return fp.read().strip().splitlines()
-
-requirements = read_requirements('requirements.txt')
+# with open("requirements.txt", "r", encoding="utf-8") as fp:
+#     requirements = fp.read().strip().splitlines()
 
 setup(
-    name='spotify-image-py',
-    version='1.0',
-    author='KarlHlmgrn',
-    author_email='karlhlmgrn.github@gmail.com',
-    description='Simple library to get currently playing song from Spotify, optimal for small projects',
-    install_requires=requirements,
+    name="spotify-image-py",
+    version="1.0",
+    author="KarlHlmgrn",
+    author_email="karlhlmgrn.github@gmail.com",
+    description="Simple library to get currently playing song from Spotify, optimal for small projects",
+    install_requires=[
+        "Flask >= 2.2.2",
+        "Pillow >= 9.2.0",
+        "qrcode >= 7.3.1",
+        "requests >= 2.25.1",
+        "six >= 1.16.0"
+    ],
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/KarlHlmgrn/spotify-image-py',
+    url="https://github.com/KarlHlmgrn/spotify-image-py",
     packages=find_packages()
 )
